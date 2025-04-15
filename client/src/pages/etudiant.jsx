dddd32d37206bb65caf31c1feb30b0166d43e2d1
Aya El Kart <aya.elkart.2003@gmail.com>
2025-04-16 01:19:03 +0200

    Reorgnized the `etudiant` page and the css

--- client/src/pages/etudiant.jsx
+++ client/src/pages/etudiant.jsx
@@ -802,6 +802,10 @@ const Etudiant = () => {
                           className="clear-cart-btn"
                           onClick={clearCart}
                         >
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <polyline points="3 6 5 6 21 6"></polyline>
+                            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                          </svg>
                           Clear Cart
                         </button>
                         <button
@@ -812,6 +816,10 @@ const Etudiant = () => {
                           }}
                         >
                           Checkout
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <line x1="5" y1="12" x2="19" y2="12"></line>
+                            <polyline points="12 5 19 12 12 19"></polyline>
+                          </svg>
                         </button>
                       </div>
                     </>
@@ -1039,14 +1047,6 @@ const Etudiant = () => {
                             </button>
                           </div>
                         ))}
-
-                        <button
-                          type="button"
-                          className="edit-cart-btn"
-                          onClick={() => setActiveView('browse')}
-                        >
-                          Edit Cart
-                        </button>
                       </div>
                     )}
                   </div>
