9450b153b100680e79c2122ba5e310057ddc5fb9
Aya El Kart <aya.elkart.2003@gmail.com>
2025-04-20 00:49:09 +0200

    Updated the logo's path for email sending

--- server/utilities/templates/contactEmail.js
+++ server/utilities/templates/contactEmail.js
@@ -34,8 +34,7 @@ const generateContactEmail = (data) => {
                 <tr>
                   <td style="padding: 30px 0; text-align: center; background: linear-gradient(135deg, #6c2bd9, #ff6a00);">
                     <div style="display: inline-block;">
-                      <img src="http://localhost:3000/favicon.svg" alt="Logo GIMS" style="vertical-align: middle; height: 32px; margin-right: 10px;">
-                      <h1 style="color: white; font-size: 28px; margin: 0; letter-spacing: -0.02em; font-weight: 800; display: inline-block; vertical-align: middle;">GIMS<span style="color: white; font-size: 32px;">.</span></h1>
+                      <img src="http://localhost:3000/logo_main_dark.svg" alt="Logo GIMS" style="vertical-align: middle; height: 50px; margin-right: 10px;">
                     </div>
                   </td>
                 </tr>
