42e02674cc3a9fd4bd60cd948557afaef01f1e96
Aya El Kart <aya.elkart.2003@gmail.com>
2025-05-04 14:44:18 +0100

    Corrected the 'mot de passe oublié' css

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -163,7 +163,7 @@ const Login = () => {
                   onChange={(e) => setRememberMe(e.target.checked)}
                 /> Se souvenir de moi
               </label>
-              <button className="forgot-link" onClick={handleForgotPassword}>Mot de pass oublié?</button>
+              <button className="secondary-button" onClick={handleForgotPassword}>Mot de pass oublié?</button>
             </div>
             <button type="submit" disabled={loading} className="login-button">
               {loading ? 'Authentification en cours...' : 'Login'}
